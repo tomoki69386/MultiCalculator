2e946cc3ed61187a02e697fe53433221939675c7
ともき <tomoki_69386@yahoo.co.jp>
2020-09-20 17:20:00 +0900

    App clips (#28)

---
+++ AppClips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppClips
+//
+//  Created by 築山朋紀 on 2020/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
